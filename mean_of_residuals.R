a129fe53c1dd1d928a059df0a11670f784b4e096
FirstWisdom <jason.zelaya21@yahoo.com>
2019-05-11 08:24:34 -0700

    added note about two excluded dummy variable columns

--- mean_of_residuals.R
+++ mean_of_residuals.R
@@ -1,7 +1,7 @@
 ## Import the csv and make sure the data does not have the character vectors converted to factors
 df = read.csv('insurance_cleaned.csv', !stringsAsFactors)
 
-## Build the linear model
+## Build the linear model. Exclude the sex_male and smoker_yes columns because they are dummy variables
 mod <- lm(charges ~ age + bmi + sex_female + smoker_no + region_northeast + region_northwest + region_southeast + region_southwest, data = df)
 
 ## Calculate the mean of residuals to determine whether or not the assumption that the mean of residuals is close to zero holds true
