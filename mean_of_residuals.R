83279e116a343d55d5cd56afe137266e83a7d371
FirstWisdom <jason.zelaya21@yahoo.com>
2019-05-11 07:55:14 -0700

    added notes and the mean of residuals value to the .R file

--- mean_of_residuals.R
+++ mean_of_residuals.R
@@ -1,3 +1,9 @@
-df = read.csv('insurance_cleaned.csv', stringsAsFactors = FALSE)
+## Import the csv and make sure the data does not have the character vectors converted to factors
+df = read.csv('insurance_cleaned.csv', !stringsAsFactors)
+## Build the linear model
 mod <- lm(charges ~ age + bmi + sex_female + smoker_no + region_northeast + region_northwest + region_southeast + region_southwest, data = df)
+## Calculate the mean of residuals to determine whether or not the assumption that the mean of residuals is close to zero holds true
 mean(mod$residuals)
+
+## Result: -5.173234e-13
+## Conclusion: The assumption that the mean of residuals is close to zero holds true
